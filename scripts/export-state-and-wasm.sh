7053f25d8cca7982743a9a9e33d671d3ff80eafe
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-04-09 12:39:30 +0300

    Fix wasm file name in export script

--- scripts/export-state-and-wasm.sh
+++ scripts/export-state-and-wasm.sh
@@ -8,7 +8,7 @@ PARACHAIN_ID=$1
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 BIN_PATH=$PROJECT_DIR/target/release/subsocial-collator
 
-WASM_PATH=$PROJECT_DIR/para-$PARACHAIN_ID-wasm
+WASM_PATH=$PROJECT_DIR/para-$PARACHAIN_ID.wasm
 GENESIS_PATH=$PROJECT_DIR/para-$PARACHAIN_ID-genesis
 
 [[ -f $WASM_PATH ]] && rm -f $WASM_PATH
